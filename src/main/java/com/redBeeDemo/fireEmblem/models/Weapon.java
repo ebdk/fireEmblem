0a5105387be8dbbcf7d0e56ffb93118fea87ab87
Esteban Bedecarats <edbedecarats@gmail.com>
2018-05-05 15:27:24 -0300

    Modifcada la estructura de las debilidades

--- src/main/java/com/redBeeDemo/fireEmblem/models/Weapon.java
+++ src/main/java/com/redBeeDemo/fireEmblem/models/Weapon.java
@@ -65,6 +65,12 @@ public class Weapon {
 		this.weakAgainst = weakAgainst;
 	}
 	
-	
+	  enum SwAxSp {
+		    Sw("Sword", "Ax"), Sp("Spear", "Sw"), Ax("Axe", "Sp");
+		    private final String beats, name;
+		    SwAxSp(String name, String beats) { this.beats = beats; this.name = name; }
+		    int compare(SwAxSp other) { return other == this? 0 : other == valueOf(beats)? 1 : -1; }
+		    String fullName() { return name; }
+		  }
 	
 }
